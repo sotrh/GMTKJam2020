b1beff4816fda129b4289c23174d240bf5a372e1
Ben Hansen <bhbenjaminhansen@gmail.com>
2020-07-10 22:55:52 -0600

    fixed rotation breaking

--- Maze.gd
+++ Maze.gd
@@ -14,13 +14,34 @@ enum Orientation {
 export(Orientation) var current_orientation
 export(Orientation) var desired_orientation
 
+func _input(event):
+	if Input.is_action_just_pressed("ui_up"):
+		match desired_orientation:
+			Orientation.DEG0:
+				desired_orientation = Orientation.DEG90
+			Orientation.DEG90:
+				desired_orientation = Orientation.DEG180
+			Orientation.DEG180:
+				desired_orientation = Orientation.DEG270
+			Orientation.DEG270:
+				desired_orientation = Orientation.DEG0
+	if Input.is_action_just_pressed("ui_down"):
+		match desired_orientation:
+			Orientation.DEG0:
+				desired_orientation = Orientation.DEG270
+			Orientation.DEG90:
+				desired_orientation = Orientation.DEG0
+			Orientation.DEG180:
+				desired_orientation = Orientation.DEG90
+			Orientation.DEG270:
+				desired_orientation = Orientation.DEG180
 
 func _physics_process(delta):
 	if current_orientation != desired_orientation:
 		var dr = desired_orientation - rotation_degrees
 		var rotation_direction = sign(dr)
 		print(dr)
-		if abs(dr) < 0.000001:
+		if abs(dr) < 0.0001:
 			rotation_degrees = desired_orientation
 			current_orientation = desired_orientation
 		else:
